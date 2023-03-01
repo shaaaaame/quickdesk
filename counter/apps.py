269293f89306eb7e7785df9f5226174ea6abd34b
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2023-03-01 13:03:42 -0500

    revamped proj structure

--- counter/apps.py
+++ counter/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CounterManagementConfig(AppConfig):
+class CounterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'counter_management'
+    name = 'counter'
